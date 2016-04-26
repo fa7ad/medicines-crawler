00ab4c249fdee7a2c5ef13171435f680855a236c
Fahad Hossain <evilinfinite7@gmail.com>
2016-04-26 17:11:19 +0600

    change from csv to json

--- crawler.py
+++ crawler.py
@@ -1,42 +1,74 @@
+#!/usr/bin/env python3
+"""
+Crawls Square pharma and makes a JSON from all their meds
+"""
+
+from string import ascii_uppercase as alphabet
 from bs4 import BeautifulSoup
-import requests
-import string
-import csv
+from requests import get as http_get
 
-# ASCII charecter uppercase list
-charecter = list(string.ascii_uppercase)
-sum = 0
 
-# Create an object of file
-file_obj = open('squarepharma.csv', "w") # File open
-# Write the Pharmaceuticals name
-file_obj.write("Square Pharmaceuticals Ltd.\n\n")
-# Write medicine name header
-file_obj.write("Medicine Name,")
-# Write Company name header
-file_obj.write("Company Name\n")
+def crawl(characters):
+    """
+    Makes a JSON file of all the medicines from Square
 
-print("Company ----- Medicine name --------------- Group")
-print("=============================================================================")
+    Args:
+        characters: list of characters to search in square's database
 
-for char in charecter:
-	# URL with trade and char query params
-	url = "http://www.squarepharma.com.bd/products-by-tradename.php?type=trade&char="+char
-	# Get the requested URL
-	req  = requests.get(url)
-	# Passing the requested content to Beautiful Soup
-	soup = BeautifulSoup(req.content)
-	# Targeting data/html content
-	data = soup.find_all("div", {"class":"products-holder"})
+    Returns:
+        A two item tuple of output formatted strings and total number of meds
+    """
+    output = "Square Pharmaceuticals Ltd.\n"
+    total = 0
 
-	for item in data:
-		print("Square ----- " +item.contents[2].text+ " ----- " + item.contents[4].text)
-		sum +=1 # Count total number of medicine
-		print("=============================================================================")
+    # Create an object of file
+    with open('square_pharma.json', "w") as file_obj:
+        file_obj.write('{')
+        # write metadata
+        file_obj.write('''"meta": {
+                "company": "Square Pharmaceuticals Ltd."
+            },''')
 
-		# Writing the medicine information into CSV file
-		file_obj.write(item.contents[2].text.encode("utf-8")+',')
-		file_obj.write(item.contents[4].text.encode("utf-8")+"\n")
+        # make the medicines object
+        file_obj.write('"medicines": [')
 
-file_obj.close() # File close
-print("Total medicines :  {0}".format(sum)) # Print the total number of medicines
+        output += """
+        Company \t Brand Name \t Group
+        ======================================"""
+
+        url = "http://squarepharma.com.bd/products-by-tradename.php?type=trade&char="
+        for character in characters:
+            url += character
+
+            # Get the requested URL
+            req = http_get(url)
+
+            # Passing the requested content to Beautiful Soup
+            soup = BeautifulSoup(req.content, 'html.parser')
+
+            # Targeting data/html content
+            items = soup.find_all("div", {"class":"products-holder"})
+            total += len(items)
+
+
+            for item in items:
+                med_name = item.contents[2].text
+                med_group = item.contents[4].text
+
+                output_line = "Square \t " + med_name + " \t " + med_group
+                output += output_line
+
+                #write meds to json
+                file_obj.write('''{
+                        "trade_name": "%s",
+                        "group_name": "%s"
+                    },''' % (med_name.encode('utf-8'), med_group.encode('utf-8')))
+
+        file_obj.write(']}')
+    return (output, total)
+
+
+if __name__ == "__main__":
+    (CRAWL_RESULT, COUNT) = crawl(list(alphabet))
+    print(CRAWL_RESULT)
+    print("Total number of meds:", COUNT)
