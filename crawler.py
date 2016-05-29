6e89bdb04b21ac35823e046bd7f6b3e43460114f
Fahad Hossain <oneloopone@gmail.com>
2016-05-29 06:42:37 +0600

    updated with a proper specification of parsers
    TODO: implement incepta next

--- crawler.py
+++ crawler.py
@@ -1,71 +1,54 @@
 #!/usr/bin/env python3
 """
-Crawls Square pharma and makes a JSON from all their meds
+Crawls parmaceutical companies and makes a JSON from all their meds
 """
 
 from string import ascii_uppercase as alphabet
-from bs4 import BeautifulSoup
-from requests import get as http_get
+from pick import pick
+import json
+import re
 
 
-def crawl(characters):
+def main():
+    """CLI launcher for the crawler"""
+    title = "Choose a pharpaceutical company."
+    options = ["Square"]
+    indicator = '>>>'
+    option, _ = pick(options, title, indicator=indicator)
+    if re.search('Square', option):
+        from parsers.square_pharma import square_parser as company_parser
+        print('Data from "Square" will be recoded to "square_pharma.json".')
+        print('Please wait.')
+        count = download_data(
+            file_name = 'square_pharma',
+            company = 'Square Pharmaceuticals Ltd.',
+            link_modifiers = list(alphabet),
+            website = 'http://squarepharma.com.bd',
+            parser = company_parser)
+        print('Downloaded {0:d} records from {1!s}'.format(count, option))
+
+
+def download_data(
+    file_name = 'square_pharma',
+    company = 'Square Pharmaceuticals Ltd.',
+    link_modifiers = list(alphabet),
+    website = 'http://squarepharma.com.bd',
+    parser=None
+    ):
+    """Download and store pharmaceutical data from a provider using given parser
     """
-    Makes a JSON file of all the medicines from Square
+    with open('{!s}.json'.format(file_name), "w") as file_obj:
 
-    Args:
-        characters: list of characters to search in square's database
-
-    Returns:
-        The total number of meds
-    """
-    print("Square Pharmaceuticals Ltd.")
-    total = 0
-
-    # Create an object of file
-    with open('square_pharma.json', "w") as file_obj:
-        file_obj.write('{')
-        # write metadata
-        file_obj.write('''"meta": {
-                "company": "Square Pharmaceuticals Ltd."
-            },''')
-
-        # make the medicines object
-        file_obj.write('"medicines": [')
-
-        print("""Company \t Brand Name \t\t\t Group
-=====================================================""")
-
-        url = "http://squarepharma.com.bd/products-by-tradename.php?type=trade&char="
-        for character in characters:
-            link = url + character
-
-            # Get the requested URL
-            req = http_get(link)
-
-            # Passing the requested content to Beautiful Soup
-            soup = BeautifulSoup(req.content, 'html.parser')
-
-            # Targeting data/html content
-            items = soup.find_all("div", {"class":"products-holder"})
-            total += len(items)
-
-
-            for item in items:
-                med_name = item.contents[2].text
-                med_group = item.contents[4].text
-
-                print("Square \t {} \t\t\t {}".format(med_name, med_group))
-
-                #write meds to json
-                file_obj.write('''{
-                        "trade_name": "%s",
-                        "group_name": "%s"
-                    },''' % (med_name, med_group))
-
-        file_obj.write('{}]}')
-    return total
+        (company_meds, total) = parser(link_modifiers)
+        company = {
+            'company': '{!s}'.format(company),
+            'website': website,
+            'medicine_count': total,
+            'medicines': company_meds
+        }
+        file_obj.write(json.dumps(company, indent=4))
+    return len(company_meds)
 
 
 if __name__ == "__main__":
-    COUNT = crawl(list(alphabet))
-    print("Total number of meds:", COUNT)
+    main()
---
+++ parsers/square_pharma.py
@@ -0,0 +1,37 @@
+"""
+Parser for square pharmaceuticals
+"""
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import requests
+
+
+def square_parser(characters):
+    """Parse records from square pharma
+
+    Args:
+        characters: characters to loop through the url
+    
+    Returns:
+        2 item tuple containing all the meds as a list and a count of all meds
+    """
+    link = 'http://squarepharma.com.bd/products-by-tradename.php?type=trade&char={0!s}'
+    meds = []
+
+    for character in characters:
+        url = link.format(character)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        items = soup.find_all("div", {"class": "products-holder"})
+
+        for item in items:
+            med_name = item.contents[2].text
+            med_group = item.contents[4].text
+            med = {
+                'trade_name': med_name,
+                'group_name': med_group
+            }
+            meds.append(med)
+
+    return (meds, len(meds))
